6422b756c01929acf184d35730412ebfb389f310
Sean Haneberg <haneberg@gmail.com>
2018-09-15 22:01:34 -0700

    check for array length

--- src/CellManager.test.js
+++ src/CellManager.test.js
@@ -3,5 +3,5 @@ import CellManager from './models/CellManager';
 it('calls getNeighborStates', () => {
   const manager = new CellManager(10, 10);
   const results = manager.getNeighborStates(0, 0);
-  // TOOD: Check if results is an array of length 8
+  expect(results.length).toBe(8);
 });
