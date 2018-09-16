b7e4715383298a0561df02a7add96585df094b61
Sean Haneberg <haneberg@gmail.com>
2018-09-15 21:51:05 -0700

    getNeighborStates populates array

--- src/models/CellManager.js
+++ src/models/CellManager.js
@@ -1,17 +1,6 @@
-// A look-less model of a Conway's Life cell
 
 class CellManager {
 
-  /*
-   *  Array indicies of X's neighbors
-   *  +---+---+---+
-   *  | 0 | 1 | 2 |
-   *  |-------|---|
-   *  | 3 | X | 4 |
-   *  |-------|---|
-   *  | 5 | 6 | 7 |
-   *  +-----------+
-   */
 
   constructor(width, height) {
     let cells = [];
@@ -30,9 +19,48 @@ class CellManager {
     this.cells = cells;
   }
 
+  /*
+   *  Array indicies of X's neighbors
+   *  +---+---+---+
+   *  | 0 | 1 | 2 |
+   *  |-------|---|
+   *  | 3 | X | 4 |
+   *  |-------|---|
+   *  | 5 | 6 | 7 |
+   *  +-----------+
+   */
+
   getNeighborStates(x, y) {
+    // Note that the edge of the grid will come back as "not alive"
     // TODO: Return an array of the neighboring states
-    return [];
+
+    const left = x - 1;
+    const leftInBound = (left >= 0);
+
+    const right = x + 1;
+    const rightInBound = (right < this.cells.length);
+
+    const up = y - 1;
+    const upInBound = (up >= 0);
+
+    const down = y + 1;
+    const downInBound = (down < this.cells[x].length);
+
+    return [
+      // row above 
+      leftInBound && upInBound ? this.cells[left][up] : false,
+      upInBound ? this.cells[x][up] : false,
+      rightInBound && upInBound ? this.cells[right][up] : false,
+
+      // main row
+      leftInBound ? this.cells[left][y] : false,
+      rightInBound ? this.cells[right][y] : false,
+      
+      // row below
+      leftInBound && downInBound ? this.cells[left][down] : false,
+      downInBound ? this.cells[x][down] : false,
+      rightInBound && downInBound ? this.cells[right][down] : false,
+    ];
   }
 }
 
