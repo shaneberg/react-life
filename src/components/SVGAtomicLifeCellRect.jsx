dc6493f2d75b2be05524776469b4e7ae8c20faef
Sean Haneberg <haneberg@gmail.com>
2019-01-20 14:13:12 -0800

    only render living cells

--- src/components/SVGAtomicLifeCellRect.jsx
+++ src/components/SVGAtomicLifeCellRect.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-// import './App.css';
 class SVGAtomicLifeCellRect extends Component {
   render() {
-    let fill = this.props.alive ? "blue" : "white";
-    return (
+    let fill = "blue";
+    const rect =
       <rect
         x={this.props.x}
         y={this.props.y}
         width={this.props.cellRadius * 2}
         height={this.props.cellRadius * 2}
-        fill={fill}/>
-    );
+        fill={fill}
+      />;
+    const toRender = this.props.alive ? rect : null;
+    return (toRender);
   }
 }
 
